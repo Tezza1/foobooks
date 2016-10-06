01f796b8ffe68be5b7fb7675d74715caf7fce844
Susan Buck <susan@susanbuck.net>
2016-10-06 14:55:19 -0400

    Set up a practice controller and routes for use in lecture

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,16 @@ Route::get('/faq', 'PageController@faq')->name('page.faq');
 Route::get('/contact', 'ContactController')->name('contact');
 
 
+
+/**
+* A quick and dirty way to set up a whole bunch of practice routes
+* that I'll use in lecture.
+*/
+Route::get('/practice', 'PracticeController@index')->name('practice.index');
+for($i = 0; $i < 100; $i++) {
+    Route::get('/practice/'.$i, 'PracticeController@example'.$i)->name('practice.example'.$i);
+}
+
 /**
 * Main homepage
 */
